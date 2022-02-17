a81a2891ef84dce178f41d0925b592082a174339
mavinsson <98565656+mavinsson@users.noreply.github.com>
2022-02-17 17:08:34 -0600

    Create todaslasvariables.cpp

---
+++ todaslasvariables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    bool valorboolean;
+    valorboolean = true;
+
+    int valorentero = 15;
+    double valordouble = 20.99;
+    string valorstring = "hola como estan";
+    char valorchar = 'b';
+      
+    cout << "Valor Boolean: " << valorboolean << endl;
+	cout << "Valor entero: " << valorentero << endl;
+	cout << "Valor double: " << valordouble << endl;
+	cout << "valor String: " <<valorstring << endl;
+	cout << "Valor Char: " << valorchar << endl;
+    
+    			
+    return 0;
+}
